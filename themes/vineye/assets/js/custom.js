08b3fd83d1921f6b9d8efc7a422de4a3fe9cb951
TriNguyen <tringuyen.2372004@gmail.com>
2025-09-20 15:59:18 +0700

    commit-delete-css-js-2

--- themes/vineye/assets/js/custom.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("Page loaded!");
-    let elements = document.querySelectorAll(".btn");
-    elements.forEach(element => {
-        element.addEventListener("click", function() {
-            alert("Button clicked!");
-        });
-    });
-});
