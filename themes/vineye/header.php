3030f75a6388334277eba87c217ccfa339b05d09
TriNguyen <tringuyen.2372004@gmail.com>
2025-09-24 11:54:54 +0700

    commit-fix-css-nav-menu-mobile-8

--- themes/vineye/header.php
+++ themes/vineye/header.php
@@ -53,24 +53,9 @@
                                 aria-label="<?php esc_attr_e('Mở menu','vineye'); ?>">
                                 <span class="bar"></span><span class="bar"></span><span class="bar"></span>
                             </button>
-                            <!-- Nút mở menu mobile -->
-                            <button class="mobile-nav-toggle d-lg-none ms-auto me-0" type="button"
-                                aria-controls="mobileNav" aria-expanded="false"
-                                aria-label="<?php esc_attr_e('Mở menu','vineye'); ?>">
-                                <span class="bar"></span><span class="bar"></span><span class="bar"></span>
-                            </button>
 
-                            <!-- Language (mobile only) -->
-                            <?php if ( function_exists('pll_the_languages') ) : ?>
-                            <div class="lang-switch d-lg-none" aria-label="Language switcher">
-                                <?php pll_the_languages([
-                                'dropdown'       => 0,           // hiện dạng list đơn giản
-                                'show_flags'     => 1,           // cờ
-                                'show_names'     => 1,           // tên ngôn ngữ
-                                'hide_if_empty'  => 0,
-                                ]); ?>
-                            </div>
-                            <?php endif; ?>
+
+
 
 
                             <!-- Hotline DESKTOP -->
@@ -120,6 +105,17 @@
                                     <a class="btn-hotline" href="tel:19009140">1900 9140</a>
                                 </div>
                             </aside>
+                            <!-- Language (mobile only) -->
+                            <?php if ( function_exists('pll_the_languages') ) : ?>
+                            <div class="lang-switch d-lg-none" aria-label="Language switcher">
+                                <?php pll_the_languages([
+                                'dropdown'       => 0,           // hiện dạng list đơn giản
+                                'show_flags'     => 1,           // cờ
+                                'show_names'     => 1,           // tên ngôn ngữ
+                                'hide_if_empty'  => 0,
+                                ]); ?>
+                            </div>
+                            <?php endif; ?>
 
                         </div><!-- /.header-tools -->
                     </div>
